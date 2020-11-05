2db39223afed514aa727b4365661ca01e4f1ae9b
Samuel Martins <sfelix_martins@hotmail.com>
2020-11-05 08:19:11 -0300

    test: uncomment delete test from file manager

--- src/__tests__/core/project/persistence/manager/file-manager/delete.spec.ts
+++ src/__tests__/core/project/persistence/manager/file-manager/delete.spec.ts
@@ -1,84 +1,83 @@
 import * as fs from 'fs';
 
 import { FileManager } from '../../../../../../core/project/persistence/manager/file.manager';
-import { Project } from '../../../../../../core/project/project.entity';
-// import { assertCreatedFileWithEmptyContent } from './common-asserts';
+import { assertCreatedFileWithEmptyContent } from './common-asserts';
 
 const fixturesPath = `${__dirname}/../../../../../fixtures`;
 
 describe('File manager', () => {
   describe('Delete', () => {
-    // describe('When folder not exists', () => {
-    //   const path = `${fixturesPath}/not-existent-folder`;
-    //   const projectsFile = `${path}/projects.json`;
-    //   const alias = 'funny';
+    describe('When folder not exists', () => {
+      const path = `${fixturesPath}/not-existent-folder`;
+      const projectsFile = `${path}/projects.json`;
+      const alias = 'funny';
 
-    //   let result: boolean;
+      let result: boolean;
 
-    //   beforeAll(() => {
-    //     const fileManager = new FileManager(path);
+      beforeAll(() => {
+        const fileManager = new FileManager(path);
 
-    //     result = fileManager.delete(alias);
-    //   });
+        result = fileManager.delete(alias);
+      });
 
-    //   afterAll(() => {
-    //     fs.unlinkSync(projectsFile);
-    //     fs.rmdirSync(path);
-    //   });
+      afterAll(() => {
+        fs.unlinkSync(projectsFile);
+        fs.rmdirSync(path);
+      });
 
-    //   it('should return undefined', () => {
-    //     expect(result).toBe(false);
-    //   });
+      it('should return undefined', () => {
+        expect(result).toBe(false);
+      });
 
-    //   assertCreatedFileWithEmptyContent(projectsFile);
-    // });
+      assertCreatedFileWithEmptyContent(projectsFile);
+    });
 
-    // describe('When folder already exists', () => {
-    //   const path = `${fixturesPath}/projects-folder`;
-    //   const projectsFile = `${path}/projects.json`;
+    describe('When folder already exists', () => {
+      const path = `${fixturesPath}/projects-folder`;
+      const projectsFile = `${path}/projects.json`;
 
-    //   let result: boolean;
+      let result: boolean;
 
-    //   beforeAll(() => {
-    //     const fileManager = new FileManager(path);
+      beforeAll(() => {
+        const fileManager = new FileManager(path);
 
-    //     result = fileManager.delete('project');
-    //   });
+        result = fileManager.delete('project');
+      });
 
-    //   afterAll(() => {
-    //     fs.unlinkSync(projectsFile);
-    //   });
+      afterAll(() => {
+        fs.unlinkSync(projectsFile);
+      });
 
-    //   it('should return undefined', () => {
-    //     expect(result).toBe(false);
-    //   });
+      it('should return undefined', () => {
+        expect(result).toBe(false);
+      });
 
-    //   assertCreatedFileWithEmptyContent(projectsFile);
-    // });
+      assertCreatedFileWithEmptyContent(projectsFile);
+    });
 
-    // describe('When projects file already exists', () => {
-    //   const path = `${fixturesPath}/projects-file`;
-    //   const projectsFile = `${path}/projects.json`;
+    describe('When projects file already exists', () => {
+      const path = `${fixturesPath}/projects-file`;
+      const projectsFile = `${path}/projects.json`;
 
-    //   let result: boolean;
+      let result: boolean;
 
-    //   beforeAll(() => {
-    //     const fileManager = new FileManager(path);
+      beforeAll(() => {
+        const fileManager = new FileManager(path);
 
-    //     result = fileManager.delete('project');
-    //   });
+        result = fileManager.delete('project');
+      });
 
-    //   afterAll(() => {
-    //     // Reset file
-    //     fs.writeFileSync(projectsFile, JSON.stringify({ projects: [] }));
-    //   });
+      afterAll(() => {
+        // Reset file
+        fs.writeFileSync(projectsFile, JSON.stringify({ projects: [] }));
+      });
 
-    //   it('should return undefined', () => {
-    //     expect(result).toBe(false);
-    //   });
+      it('should return undefined', () => {
+        expect(result).toBe(false);
+      });
 
-    //   assertCreatedFileWithEmptyContent(projectsFile);
-    // });
+      assertCreatedFileWithEmptyContent(projectsFile);
+    });
 
     describe('When already have projects created', () => {
       const path = `${fixturesPath}/unordered-projects-list`;
